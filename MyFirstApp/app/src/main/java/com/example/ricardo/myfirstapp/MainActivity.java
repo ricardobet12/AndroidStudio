da941da38538b4b9e4c3aacdbbf9c12684fe63ab
Ricardo <ricardo.bet1209@gmail.com>
2018-11-23 11:44:53 -0500

    Pasar datos de xml al activity

---
+++ MyFirstApp/app/src/main/java/com/example/ricardo/myfirstapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.ricardo.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
+    private EditText et2;
+    private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toast.makeText(this,"OnCreate",Toast.LENGTH_LONG).show();
+
+        et1 = (EditText)findViewById(R.id.txtNum1);
+        et2 = (EditText)findViewById(R.id.txtNum2);
+        tv1 = (TextView)findViewById(R.id.txtResultado);
+    }
+
+    public void sumar(View view){
+
+    }
+
+    /*
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(this,"OnStart",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this,"OnResume",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(this,"OnPause",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(this,"OnStop",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(this,"OnDestroy",Toast.LENGTH_LONG).show();
+    }
+    */
+}
